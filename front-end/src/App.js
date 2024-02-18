3e64d81427e408c4fcb0056207170e6d232d5c37
rathinakumar46 <rathinakumar825@gmail.com>
2024-02-18 22:33:49 +0530

    updated

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -36,7 +36,7 @@ function App() {
   function send()
   {
     setstatus(true)
-    axios.post("http://localhost:5000/mail",{msg:msg, emailList:emailList}).then(function(data)
+    axios.post("http://bulkmail-mern-stack.vercel.app/mail",{msg:msg, emailList:emailList}).then(function(data)
     {
       if(data.data === true)
       {
